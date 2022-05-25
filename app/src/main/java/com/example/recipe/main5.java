967a423930616b1f580a6af4292f812436a0c7e8
willGitAcct <willjhub@outlook.com>
2022-05-24 21:54:30 -0400

    added activities, images, updated menu for the main course dishes

---
+++ app/src/main/java/com/example/recipe/main5.java
@@ -0,0 +1,14 @@
+package com.example.recipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class main5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+    }
+}
--- app/src/main/java/com/example/recipe/mainCourseMenuActivity.java
+++ app/src/main/java/com/example/recipe/mainCourseMenuActivity.java
@@ -11,8 +11,8 @@ public class mainCourseMenuActivity extends AppCompatActivity implements Recycle
 
     RecyclerView recyclerView;
     RecyclerAdapter recyclerAdapter;
-    String[] foodNames = {"Eggs n Bacon", "pfft pfft", "pfft pfft","pfft pfft","pfft pfft"};
-    int[] images = {R.drawable.breakfast_choice, R.drawable.dessert_choice, R.drawable.main_choice, R.drawable.salad_choice, R.drawable.dessert_choice};
+    String[] foodNames = {"Pasta Arrabiata", "Garlic Chicken Fried Brown Rice", "Homemade Mac and Cheese","Pan con Milanesa","Quick Beef Stir-Fry"};
+    int[] images = {R.drawable.main_choice, R.drawable.main_garlic, R.drawable.main_mac, R.drawable.main_milanesa, R.drawable.main_stirfry};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +24,23 @@ public class mainCourseMenuActivity extends AppCompatActivity implements Recycle
                 Intent mIntent;
                 switch (index) {
                     case 0: //first item in Recycler view
-                        mIntent = new Intent(getApplicationContext(), break1.class);
+                        mIntent = new Intent(getApplicationContext(), main1.class);
                         startActivity(mIntent);
                         break;
                     case 1: //second item in Recycler view
-                        mIntent = new Intent(getApplicationContext(), break1.class);
+                        mIntent = new Intent(getApplicationContext(), main2.class);
                         startActivity(mIntent);
                         break;
                     case 2: //third item in Recycler view
-                        mIntent = new Intent(getApplicationContext(), break1.class);
+                        mIntent = new Intent(getApplicationContext(), main3.class);
+                        startActivity(mIntent);
+                        break;
+                    case 3: //third item in Recycler view
+                        mIntent = new Intent(getApplicationContext(), main4.class);
                         startActivity(mIntent);
                         break;
                     default:
-                        mIntent = new Intent(getApplicationContext(), break1.class);
+                        mIntent = new Intent(getApplicationContext(), main5.class);
                         startActivity(mIntent);
                 }
             }
@@ -50,23 +54,7 @@ public class mainCourseMenuActivity extends AppCompatActivity implements Recycle
     }
     @Override
     public void onClickedPic(int index) {
-        Intent mIntent;
-        switch (index) {
-            case 0: //first item in Recycler view
-                mIntent = new Intent(getApplicationContext(), break1.class);
-                startActivity(mIntent);
-                break;
-            case 1: //second item in Recycler view
-                mIntent = new Intent(getApplicationContext(), break1.class);
-                startActivity(mIntent);
-                break;
-            case 2: //third item in Recycler view
-                mIntent = new Intent(getApplicationContext(), break1.class);
-                startActivity(mIntent);
-                break;
-            default:
-                mIntent = new Intent(getApplicationContext(), break1.class);
-                startActivity(mIntent);
-        }
+
+
     }
 }
